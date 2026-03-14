c6f5e241187214fb06ea0b83b65d1d7fefa07b4f
Korobokkk <bbb754748@gmail.com>
2026-03-14 21:59:09 +0300

    fix test.cpp

--- clang/test/compiler-course/kiselev_i_first_lab/test.cpp
+++ clang/test/compiler-course/kiselev_i_first_lab/test.cpp
@@ -1,60 +1,45 @@
-// RUN: %clang_cc1 -load %llvmshlibdir/kiselev_i_first_lab_ClangAST%pluginext -plugin override_check -fsyntax-only %s 2>&1 | FileCheck %s
+// RUN: split-file %s %t
+// RUN: %clang_cc1 -load %llvmshlibdir/kiselev_i_first_lab_ClangAST%pluginext -plugin override_check -fsyntax-only -verify %t/with_warnings.cpp
+// RUN: %clang_cc1 -load %llvmshlibdir/kiselev_i_first_lab_ClangAST%pluginext -plugin override_check -fsyntax-only -verify %t/without_warnings.cpp
 
-class Base1 { 
+//--- with_warnings.cpp
+
+class Base1 {
 public:
   virtual void foo();
 };
 
 class Derived1 : public Base1 {
 public:
-  void foo() {}
+  void foo() {} // expected-warning {{method 'foo' overrides base method but is not marked 'override'}}
 };
 
-// CHECK: {{.*}}warning: method 'foo' overrides base method but is not marked 'override'
-
-class Base2 { // good case
-public:
-  virtual void foo();
-};
-
-class Derived2 : public Base2 {
-public:
-  void foo() override {}
-};
-
-// CHECK-NOT: warning: method 'foo' overrides base method but is not marked 'override'
-
-class A1 { // override with param
+class A1 {
 public:
   virtual int sum(int a, int b);
 };
 
 class B1 : public A1 {
 public:
-  int sum(int a, int b) { return a + b; }
+  int sum(int a, int b) { return a + b; } // expected-warning {{method 'sum' overrides base method but is not marked 'override'}}
 };
 
-// CHECK: {{.*}}warning: method 'sum' overrides base method but is not marked 'override'
-
-class A2 { // multiple inheritances
+class A2 {
 public:
   virtual void foo();
 };
 
 class B2 : public A2 {
 public:
-  void foo() {}
+  void foo() {} // expected-warning {{method 'foo' overrides base method but is not marked 'override'}}
 };
 
 class C2 : public B2 {
 public:
-  void foo() {}
+  void foo() {} // expected-warning {{method 'foo' overrides base method but is not marked 'override'}}
 };
 
-// CHECK: {{.*}}warning: method 'foo' overrides base method but is not marked 'override'
-// CHECK: {{.*}}warning: method 'foo' overrides base method but is not marked 'override'
-
-class L1 { // intermediate class case
+class L1 {
 public:
   virtual void f();
 };
@@ -63,12 +48,10 @@ class L2 : public L1 {};
 
 class L3 : public L2 {
 public:
-  void f() {}
+  void f() {} // expected-warning {{method 'f' overrides base method but is not marked 'override'}}
 };
 
-// CHECK: {{.*}}warning: method 'f' overrides base method but is not marked 'override'
-
-class A { // several subclass
+class A {
 public:
   virtual void f();
 };
@@ -80,12 +63,9 @@ public:
 
 class C : public A, public B {
 public:
-  void f() {}
-  void g() {}
+  void f() {} // expected-warning {{method 'f' overrides base method but is not marked 'override'}}
+  void g() {} // expected-warning {{method 'g' overrides base method but is not marked 'override'}}
 };
-
-// CHECK: {{.*}}warning: method 'f' overrides base method but is not marked 'override'
-// CHECK: {{.*}}warning: method 'g' overrides base method but is not marked 'override'
 
 class Abstract {
 public:
@@ -94,7 +74,20 @@ public:
 
 class Impl : public Abstract {
 public:
-  void run() {}
+  void run() {} // expected-warning {{method 'run' overrides base method but is not marked 'override'}}
 };
 
-// CHECK: {{.*}}warning: method 'run' overrides base method but is not marked 'override'
+
+//--- without_warnings.cpp
+
+// expected-no-diagnostics
+
+class Base2 {
+public:
+  virtual void foo();
+};
+
+class Derived2 : public Base2 {
+public:
+  void foo() override {}
+};
